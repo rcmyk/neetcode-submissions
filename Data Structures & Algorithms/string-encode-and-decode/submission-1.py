f298864672bc24611803644717f3abe895e32499
rcmyk <mehedi1778c00@gmail.com>
2026-07-25 13:26:10 +0600

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        out = ''
+        for s in strs:
+            cnt = str(len(s))
+            out += cnt
+            out += '.'
+            out += s
+        return out
+
+    def decode(self, s: str) -> List[str]:
+        i = 0
+        res = []
+        while i < len(s):
+            dot = s.index('.')
+            n = int(s[:dot])
+            s = s[dot+1:] # skip the dot
+            got = s[:n]
+            s = s[n:]
+            res.append(got)
+        return res
