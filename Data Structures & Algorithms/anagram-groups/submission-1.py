99269f106817204735f8a4b129792f2e17af8407
rcmyk <mehedi1778c00@gmail.com>
2026-07-25 12:59:37 +0600

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        out = defaultdict(list) # keyd by list as well
+        for s in strs:
+            fingerprint = [0] * 26
+            for c in s:
+                fingerprint[ord(c)-ord('a')] += 1
+            out[tuple(fingerprint)].append(s)
+        return list(out.values())
