054e8f40198cc9648c1a3785b01be5cca617e460
rcmyk <mehedi1778c00@gmail.com>
2026-07-26 08:52:28 +0600

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.py
@@ -0,0 +1,37 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])  # Path compression
+        return self.parent[x]
+
+    def union(self, x, y):
+        rx, ry = self.find(x), self.find(y)
+
+        if rx == ry:
+            return False
+
+        if self.rank[rx] < self.rank[ry]:
+            self.parent[rx] = ry
+        elif self.rank[rx] > self.rank[ry]:
+            self.parent[ry] = rx
+        else:
+            self.parent[ry] = rx
+            self.rank[rx] += 1
+
+        return True
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        # +1 because 1 based indexing,
+        # but the number of nodes and number of edges are the same (n),
+        # according to description, in tree for N node total edges N - 1
+        uf = UnionFind(len(edges) + 1)
+        for u, v in edges:
+            if not uf.union(u, v):
+                return [u, v]
+        return None
